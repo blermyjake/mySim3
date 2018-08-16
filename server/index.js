5f3cb1b7d2dc0e33dd9726d26b2e96e33a91d294
blermyjake <jeremy73120@gmail.com>
2018-08-16 15:38:05 -0500

    pt1 st1

--- server/index.js
+++ server/index.js
@@ -11,6 +11,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+massive(process.env.CONNECTION_STRING)
+  .then(sim3DB => {
+    app.set("db", sim3DB);
+  })
+  .catch(err => console.log(err));
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from "./App";
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
